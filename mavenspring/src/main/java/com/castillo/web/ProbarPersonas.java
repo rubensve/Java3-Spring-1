8e062d153b2410ec1d2247b7284796571d58c7fd
T107 <T107@PC255>
2014-06-06 21:39:43 -0500

    Se agrega la dependencia de Spring MVC

--- mavenspring/src/main/java/com/castillo/web/ProbarPersonas.java
+++ mavenspring/src/main/java/com/castillo/web/ProbarPersonas.java
@@ -1,9 +1,14 @@
 package com.castillo.web;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ProbarPersonas {
 
     public static void main(String[] args) {
-        Persona p = new CuentaChistes();
+        
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ServiciosCastillo.class);
+        Persona p = ctx.getBean(Persona.class);
         System.out.println(p.ejecutarGracia());
     }
     
---
+++ mavenspring/src/main/java/com/castillo/web/ServiciosCastillo.java
@@ -0,0 +1,14 @@
+package com.castillo.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiciosCastillo {
+    
+    @Bean
+    Persona ejecutarGracia(){
+        return new CuentaChistes();
+    }
+    
+}
